30f896b01c9d30da0824e8c97404cb3e37de9bbd
zhaojing0224 <ztomirror@gmail.com>
2023-10-07 20:24:06 +0900

    new
    new

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -2,11 +2,15 @@ package Controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import obj.UserInfoObj;
 
 /**
  * Servlet implementation class LoginController
@@ -14,29 +18,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println(1111);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/LoginPage.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		UserInfoObj userInfoObj = new UserInfoObj();
+
+		userInfoObj.setEmail(request.getParameter("email"));
+		userInfoObj.setUserId(request.getParameter("userId"));
+		userInfoObj.setPassword(request.getParameter("password"));
+		userInfoObj.setNameSei(request.getParameter("nameSei"));
+		userInfoObj.setNameMei(request.getParameter("nameMei"));
+		userInfoObj.setNameSeiKata(request.getParameter("nameSeiKata"));
+		userInfoObj.setNameMeiKata(request.getParameter("nameMeiKata"));
+
+		request.setAttribute("userInfoObj", userInfoObj);
+
+		HttpSession session = request.getSession();
+		session.setAttribute("userInfoObj", userInfoObj);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/ConfirmationPage.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 }
